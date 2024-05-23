6075a18dd5c20b261a7e468d822209c80d5a8028
Ramkumar Munisamy <mr.ramer@gmail.com>
2024-05-22 19:30:09 -0500

    My first file - py

---
+++ import.py
@@ -0,0 +1,28 @@
+import file
+import os 
+print "This is my first script"import file
+
+
+import os 
+print "This is my first script"import file
+import os 
+print "This is my first script"import file
+import os 
+print "This is my first script"
+
+
+--------------
+source code versioning tool - 
+
+test1 - ram - 
+import file
+import os 
+print "This is my first script"import file
+
+test2- ram 
+import file
+import os 
+print "This is my first script"import file
+import file
+import os 
+print "This is my first script"import file
